ea8faceb451fcfd5cda4e5b15d27335e44693bc8
Al Riddoch <alriddoch@googlemail.com>
2014-08-26 13:05:28 +0100

    Add an experimental static assertion

---
+++ dymaxion/dymaxion.cpp
@@ -0,0 +1,8 @@
+// This file may be redistributed and modified only under the terms of
+// the GNU General Public License (See COPYING for details).
+// Copyright (C) 2014 Alistair Riddoch
+
+#include <dymaxion/dymaxion.h>
+
+static_assert(sizeof(long) >= sizeof(int),
+              "int type is inexplicably larger than long.");
